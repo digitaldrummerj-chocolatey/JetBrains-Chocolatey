0ee3ad2debc8f9aa13230f394301e68aef6cdd62
jtcmedia <jtcmedia@users.noreply.github.com>
2016-03-18 11:59:08 -0700

    updated to 2016.1

--- WebStorm/chocolateyUninstall.ps1
+++ WebStorm/chocolateyUninstall.ps1
@@ -2,7 +2,7 @@ $extractionPath = (${env:ProgramFiles(x86)}, ${env:ProgramFiles} -ne $null)[0]
 
 $installDir = Join-Path $extractionPath 'JetBrains'
 
-$uninstallExe = (gci "${installDir}/WebStorm 11.0/bin/uninstall.exe").FullName | sort -Descending | Select -first 1
+$uninstallExe = (gci "${installDir}/WebStorm 2016.1/bin/Uninstall.exe").FullName | sort -Descending | Select -first 1
 
 $params = @{
 	PackageName = $packageName;
