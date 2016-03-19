6cc978122510bee6c7d559623fa62f7c5e13663a
Justin James <digitaldrummerj@gmail.com>
2016-03-18 17:31:00 -0700

    removed old change log link in nuspec.  added check to make sure that install directory was present before trying to uninstall

--- WebStorm/chocolateyInstall.ps1
+++ WebStorm/chocolateyInstall.ps1
@@ -4,4 +4,5 @@ $url64 = $url
 $silentArgs = '/S' 
 $validExitCodes = @(0) 
 
+
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
--- WebStorm/chocolateyUninstall.ps1
+++ WebStorm/chocolateyUninstall.ps1
@@ -2,13 +2,18 @@ $extractionPath = (${env:ProgramFiles(x86)}, ${env:ProgramFiles} -ne $null)[0]
 
 $installDir = Join-Path $extractionPath 'JetBrains'
 
-$uninstallExe = (gci "${installDir}/WebStorm 2016.1/bin/Uninstall.exe").FullName | sort -Descending | Select -first 1
+$installVersionDir  = Join-Path $installDir  "WebStorm 2016.1"
 
-$params = @{
-	PackageName = $packageName;
-	FileType = "exe";
-	SilentArgs = "/S";
-	File = $uninstallExe;
-}   
+if (Test-Path ($installVersionDir)) {
 
-Uninstall-ChocolateyPackage @params
+	$uninstallExe = (gci "${installDir}/WebStorm 2016.1/bin/Uninstall.exe").FullName | sort -Descending | Select -first 1
+
+	$params = @{
+		PackageName = $packageName;
+		FileType = "exe";
+		SilentArgs = "/S";
+		File = $uninstallExe;
+	}   
+
+	Uninstall-ChocolateyPackage @params
+}
